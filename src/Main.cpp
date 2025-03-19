b862e8fae56b66ab54c6f891e7a3c7daaf98cbe7
Neocrius <neocrius@gmail.com>
2025-03-19 11:53:56 +0100

    Implemented instrument settings UI

--- src/Main.cpp
+++ src/Main.cpp
@@ -11,9 +11,8 @@
 #define TOOLBAR_HEIGHT      32
 #define TOOL_BUTTON_WIDTH   32
 #define TOP_PANEL_Y         (8 + TOOLBAR_HEIGHT + 8)
-#define TOP_PANEL_H         256
+#define TOP_PANEL_H         128
 #define TOP_PANEL_W         800
-
 
 #define PATTERN_ORDER_W     (TRACK_CHECK_W * 16 + 16)
 #define PATTERN_ORDER_H     72
@@ -60,6 +59,72 @@ void DrawSongPanel(Rectangle bounds)
 
     int velocity = 100;
     GuiSpinner((Rectangle){bounds.x + PATTERN_ORDER_W + 32, bounds.y + 80, 80, 24}, "Vel. ", &velocity, 1, 127, false);
+}
+
+void DrawInstrumentSettings(Rectangle bounds)
+{
+    //draw SoundFont list
+
+#define INSTRUMENT_PROPS_W  ((TOP_PANEL_W - 24) / 2)
+#define SOUNDFONT_LIST_W    (TOP_PANEL_W - INSTRUMENT_PROPS_W - 24)
+#define SOUNDFONT_BUTTON_S  24
+
+    GuiGroupBox((Rectangle){bounds.x, bounds.y, SOUNDFONT_LIST_W, TOP_PANEL_H - 16}, "SoundFonts");
+
+    if (GuiButton((Rectangle){ bounds.x + 8, bounds.y + 8, SOUNDFONT_BUTTON_S, SOUNDFONT_BUTTON_S }, GuiIconText(ICON_FILE_OPEN, "")))
+    {
+        //Open & add a new SoundFont
+    }
+
+    if (GuiButton((Rectangle){ bounds.x + 8, bounds.y + 8 + SOUNDFONT_BUTTON_S + 4, SOUNDFONT_BUTTON_S, SOUNDFONT_BUTTON_S }, GuiIconText(ICON_CROSS, "")))
+    {
+        //Delete current SoundFont
+    }
+
+    GuiSetStyle(LISTVIEW, LIST_ITEMS_HEIGHT, 16);
+    const char* soundFonts[] = { "~/SoundFonts/ASoundFont.sf2", "~/SoundFonts/AnotherSoundFont.sf2" };
+    int sfIndex = 0;
+    int active = 0;
+    int focus = 0;
+    GuiListViewEx((Rectangle){ bounds.x + 8 + SOUNDFONT_BUTTON_S + 4, bounds.y + 8, SOUNDFONT_LIST_W - SOUNDFONT_BUTTON_S - 8 - 8 - 4, TOP_PANEL_H - 16 - 16},
+        soundFonts, 2, &sfIndex, &active, &focus);
+
+    //draw instrument properties
+
+    bounds.x += SOUNDFONT_LIST_W + 8;
+    GuiGroupBox((Rectangle){bounds.x, bounds.y, INSTRUMENT_PROPS_W, TOP_PANEL_H - 16}, "Instrument Properties");
+
+    bounds.y += 16;
+
+#define BANK_COMBO_W    64
+#define SF_COMBO_X      64
+    GuiSetStyle(LABEL, TEXT_ALIGNMENT, TEXT_ALIGN_RIGHT);
+    GuiLabel((Rectangle){ bounds.x + 8, bounds.y, SF_COMBO_X - 4, 24 }, "SoundFont");
+    int sf = 0;
+    GuiComboBox((Rectangle){ bounds.x + 8 + SF_COMBO_X, bounds.y, INSTRUMENT_PROPS_W - 16 - SF_COMBO_X, 24 }, "None", &sf);
+
+    bounds.y += 24 + 8;
+
+    GuiLabel((Rectangle){ bounds.x + 8, bounds.y, SF_COMBO_X - 4, 24 }, "Bank");
+    int bank = 0;
+    GuiComboBox((Rectangle){ bounds.x + 8 + SF_COMBO_X, bounds.y, BANK_COMBO_W, 24 }, "---", &bank);
+
+#define PATCH_COMBO_X   (8 + SF_COMBO_X + BANK_COMBO_W + 8 + 40)
+    GuiLabel((Rectangle){ bounds.x + 8 + SF_COMBO_X + BANK_COMBO_W + 8, bounds.y, 36, 24 }, "Patch");
+    int patch = 0;
+    GuiComboBox((Rectangle){ bounds.x + PATCH_COMBO_X, bounds.y, INSTRUMENT_PROPS_W - PATCH_COMBO_X - 8, 24 }, "---", &patch);
+
+    bounds.y += 24 + 8;
+ 
+    GuiLabel((Rectangle){ bounds.x + 8, bounds.y, SF_COMBO_X - 4, 24 }, "Velocity");
+    int velocity = 100;
+    GuiSpinner((Rectangle){ bounds.x + 8 + SF_COMBO_X, bounds.y, 80, 24 }, "", &velocity, 1, 200, false);
+
+    GuiLabel((Rectangle){ bounds.x + 8 + SF_COMBO_X + 80, bounds.y, 72 - 4, 24 }, "Transpose");
+    int transpose = 0;
+    GuiSpinner((Rectangle){ bounds.x + 8 + SF_COMBO_X + 80 + 72, bounds.y, 80, 24 }, "", &transpose, -48, 48, false);
+
+    GuiSetStyle(LABEL, TEXT_ALIGNMENT, TEXT_ALIGN_LEFT);
 }
 
 void DrawInstrumentList(Rectangle bounds)
@@ -162,35 +227,33 @@ int main()
                 GetColor(GuiGetStyle(DEFAULT, (int)LINE_COLOR)),
                 GetColor(GuiGetStyle(DEFAULT, (int)BACKGROUND_COLOR)));                
 
-            GuiSetState(STATE_PRESSED);
-            if (GuiButton((Rectangle){ float(tabsLeft + 8), 16, TOOL_BUTTON_WIDTH, TOOLBAR_HEIGHT },
-                GuiIconText(ICON_FILETYPE_AUDIO, "")))
+            static int currentTab = 0;
+            const int icons[] = {ICON_FILETYPE_AUDIO, ICON_FILETYPE_AUDIO, ICON_TOOLS, ICON_GEAR_BIG, ICON_FX};
+
+            for (int tab = 0; tab < 5; ++tab)
             {
-                //Switch to Song Properties
-            }
-            GuiSetState(STATE_NORMAL);
-            if (GuiButton((Rectangle){ float(tabsLeft + 8 + TOOL_BUTTON_WIDTH + 8), 16, TOOL_BUTTON_WIDTH, TOOLBAR_HEIGHT },
-                GuiIconText(ICON_FILETYPE_AUDIO, "")))
-            {
-                //Switch to SoundFont and Instrument Properties
-            }
-            if (GuiButton((Rectangle){ float(tabsLeft + 8 + 2 * (TOOL_BUTTON_WIDTH + 8)), 16, TOOL_BUTTON_WIDTH, TOOLBAR_HEIGHT },
-                GuiIconText(ICON_TOOLS, "")))
-            {
-                //Switch to Tools
-            }
-            if (GuiButton((Rectangle){ float(tabsLeft + 8 + 3 * (TOOL_BUTTON_WIDTH + 8)), 16, TOOL_BUTTON_WIDTH, TOOLBAR_HEIGHT },
-                GuiIconText(ICON_GEAR_BIG, "")))
-            {
-                //Switch to Settings
-            }
-            if (GuiButton((Rectangle){ float(tabsLeft + 8 + 4 * (TOOL_BUTTON_WIDTH + 8)), 16, TOOL_BUTTON_WIDTH, TOOLBAR_HEIGHT },
-                GuiIconText(ICON_FX, "")))
-            {
-                //Switch to FX Help
+                if (tab == currentTab) GuiSetState(STATE_PRESSED);
+                if (GuiButton((Rectangle){ float(tabsLeft + 8 + tab * (TOOL_BUTTON_WIDTH + 8)), 16, TOOL_BUTTON_WIDTH, TOOLBAR_HEIGHT },
+                    GuiIconText(icons[tab], "")))
+                {
+                    currentTab = tab;
+                }
+                GuiSetState(STATE_NORMAL);
             }
 
-            DrawSongPanel((Rectangle){16, TOP_PANEL_Y + 8, TOP_PANEL_W - 16, TOP_PANEL_H - 16});
+            Rectangle topPanelBounds = {16, TOP_PANEL_Y + 8, TOP_PANEL_W - 16, TOP_PANEL_H - 16};
+
+            switch (currentTab)
+            {
+            case 0:
+                DrawSongPanel(topPanelBounds);
+                break;
+            case 1:
+                DrawInstrumentSettings(topPanelBounds);
+                break;
+            default:
+                break;
+            }
 
             DrawInstrumentList((Rectangle){TOP_PANEL_W + 16, 8, float(instListW), TOP_PANEL_H + TOP_PANEL_Y - 8});
 
