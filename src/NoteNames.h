419d5e39c864b3232e52c76031dadd694f81a507
Neocrius <neocrius@gmail.com>
2025-04-06 12:20:42 +0200

    Updated note names with OFF instead of ---, added some #defines

--- src/NoteNames.h
+++ src/NoteNames.h
@@ -3,6 +3,9 @@
 namespace sft
 {
 
+#define NOTE_OFF			0
+#define FIRST_VALID_NOTE	21
+
 extern const char* g_noteNameSharp[127];
 extern const char* g_noteNameFlat[127];
 extern const char* g_noteNameDrums[127];
